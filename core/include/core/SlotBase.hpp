2ddb4e6417451c5687f613a553d09fcd55e3e105
Lolitron-0 <ovcharov_05@mail.ru>
2024-08-22 01:17:38 +0300

    started passes

--- core/include/core/SlotBase.hpp
+++ core/include/core/SlotBase.hpp
@@ -1,24 +1,50 @@
 #pragma once
+#include "core/Board.hpp"
 #include "core/ContiguousMatrix.hpp"
 #include "core/Symbol.hpp"
-#include "core/Board.hpp"
 #include <random>
 
 namespace plut::core {
 
+// TODO: event driven
+struct GenerationPassResult {
+  bool rerunGeneration{ false };
+};
+
+struct WinCollectionPassResult {
+  bool rerunGeneration{ false };
+  bool endWinCollection{ false };
+};
+
 class SlotBase {
 public:
+  using GenerationPass = std::function<GenerationPassResult(Board&)>;
+  using GenerationPassBuffer = std::vector<GenerationPass>;
+  using WinCollectionPass =
+      std::function<WinCollectionPassResult(Board&)>;
+  using WinCollectionPassBuffer = std::vector<WinCollectionPass>;
+
   SlotBase(std::size_t maxRows, std::size_t maxCols);
 
-  [[nodiscard]] auto getBoardState() const -> const ContiguousDynamicMatrix<Symbol>&;
+  [[nodiscard]] auto getBoardState() const
+      -> const ContiguousDynamicMatrix<Symbol>&;
   void addSymbols(std::vector<Symbol>&& symbols);
 
   void spin();
 
+protected:
+  void registerGenerationPass(const GenerationPass& newPass);
+  void registerWinCollectionPass(const WinCollectionPass& newPass);
+
+private:
+  void _resetState();
+
 private:
   Board m_Board;
   std::vector<Symbol> m_Symbols;
-	std::shared_ptr<std::mt19937_64> m_RandEngine;
+  std::shared_ptr<std::mt19937_64> m_RandEngine;
+  GenerationPassBuffer m_GenerationPasses;
+  WinCollectionPassBuffer m_WinCollectionPasses;
 };
 
 } // namespace plut::core
--- core/src/SlotBase.cc
+++ core/src/SlotBase.cc
@@ -12,10 +12,42 @@ void SlotBase::addSymbols(std::vector<Symbol>&& symbols) {
   m_Symbols = std::move(symbols);
 }
 
-void SlotBase::spin() { m_Board.resetState(); }
+void SlotBase::spin() {
+  _resetState();
+
+generation:;
+  for (auto&& generationPass : m_GenerationPasses) {
+    auto result{ std::invoke(generationPass, m_Board) };
+    if (result.rerunGeneration) {
+      goto generation;
+    }
+  }
+
+  for (auto&& winCollectionPass : m_WinCollectionPasses) {
+    auto result{ std::invoke(winCollectionPass, m_Board) };
+    if (result.rerunGeneration) {
+      goto generation;
+    }
+    if (result.endWinCollection) {
+      break;
+    }
+  }
+}
 
 auto SlotBase::getBoardState() const
     -> const ContiguousDynamicMatrix<Symbol>& {
   return m_Board.getCurrentState();
 }
+
+void SlotBase::_resetState() { m_Board.resetState(); }
+
+void SlotBase::registerGenerationPass(const GenerationPass& newPass) {
+  m_GenerationPasses.push_back(newPass);
+}
+
+void SlotBase::registerWinCollectionPass(
+    const WinCollectionPass& newPass) {
+  m_WinCollectionPasses.push_back(newPass);
+}
+
 } // namespace plut::core
